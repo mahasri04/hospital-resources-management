1c36b1ea342a345a243e026ea59f51b14728ff5a
mahasri04 <mahasrinavin@gmail.com>
2025-06-29 12:52:17 +0530

    main file

---
+++ main.py
@@ -0,0 +1,7 @@
+import tkinter as tk
+from hospital_system import HospitalResourceSystem
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = HospitalResourceSystem(root)
+    root.mainloop()
